6b3737815456d46fb3559907ab131f97e0a59fda
kihiuFrank <franklinekihiu@gmail.com>
2022-12-03 17:49:29 +0300

    contracts and deployment

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # hardhat-upgrades
 We will be working on protocols that allow users to nominate a contract / proxy address to act on behalf of another wallet address or another contract.
+
+# Tasks
+1. Upgrade Box -> BoxV2
+2. Make a Proxy Contract that will point to Box, and then later update it to point to BoxV2.
+    - Proxy -> Box
+            -> BoxV2
+# Steps
+1. Deploy a Proxy 
+    - manually 
+    - or using Hardhat deploy's built-in proxies
+    - using openzepplin's upgrade plugin
+
---
+++ deploy/01-deploy-box.js
@@ -0,0 +1,35 @@
+const { getNamedAccounts, deployments, ethers, network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    log("-------------------------------------------------------")
+    const args = []
+    const box = await ethers.getContract("Box", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+        proxy: {
+            proxyContract: "OpenZeppelinTransparentProxy",
+            viaAdminContract: {
+                name: "BoxProxyAdmin",
+                artifact: "BoxProxyAdmin",
+            },
+        },
+    })
+
+    log("-------------------------------------------------------")
+
+    //verifying the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(box.address, args)
+    }
+
+    log("-------------------------------------------------------")
+}
+module.exports.tags = ["all", "box"]
